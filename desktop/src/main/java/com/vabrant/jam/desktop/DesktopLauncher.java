77750bc6ab3b4193fcf10c6ae170689bcba5247a
John <john.developer.mail@gmail.com>
2020-03-22 23:37:43 -0700

    init

---
+++ desktop/src/main/java/com/vabrant/jam/desktop/DesktopLauncher.java
@@ -0,0 +1,28 @@
+package com.vabrant.jam.desktop;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vabrant.jam.ThermonuclearWar;
+
+/** Launches the desktop (LWJGL) application. */
+public class DesktopLauncher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static LwjglApplication createApplication() {
+		return new LwjglApplication(new ThermonuclearWar(), getDefaultConfiguration());
+	}
+
+	private static LwjglApplicationConfiguration getDefaultConfiguration() {
+		LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+		configuration.title = "ThermonuclearWar";
+		configuration.width = 640;
+		configuration.height = 480;
+		for (int size : new int[] { 128, 64, 32, 16 }) {
+			configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
+		}
+		return configuration;
+	}
+}
